c19bc173f1639592b4a69f7b45188770f41eca54
msaxton <msaxton@iliff.edu>
2019-09-16 10:15:59 -0600

    add model num variable

--- visualize_model.py
+++ visualize_model.py
@@ -1,13 +1,14 @@
 from gensim import corpora, models
 import pyLDAvis.gensim
 
-path = './gensim_output/'
+model_num = ''
+path = './gensim_output/' + model_num + '/'
 
-gensim_dictionary = corpora.Dictionary.load(path + 'gensim_dictionary.dict')
-gensim_corpus = corpora.MmCorpus(path + 'gensim_corpus.mm')
+gensim_dictionary = corpora.Dictionary.load(path + 'gensim_dictionary_' + model_num + '.dict')
+gensim_corpus = corpora.MmCorpus(path + 'gensim_corpus_' + model_num + '.mm')
 
-lda_25 = models.ldamodel.LdaModel.load(path + 'lda_25.model')
+lda_25 = models.ldamodel.LdaModel.load(path + 'lda_25_' + model_num + '.model')
 
 lda_25_viz = pyLDAvis.gensim.prepare(lda_25, gensim_corpus, gensim_dictionary)
 
-pyLDAvis.save_html(lda_25_viz, './visualizations/lda.html')
+pyLDAvis.save_html(lda_25_viz, './visualizations/lda_' + model_num + '.html')
